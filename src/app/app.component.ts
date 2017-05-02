719952ff950e0e1f7810e23ccaf610fe2e9936db
Andrea Zorzi <andrea.zorzi@moku.io>
2017-05-02 17:57:49 +0200

    jumbotron with flickity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import {MenuItem} from './menu-item';
+import {Image} from './image';
 
 
 const MENU_ITEMS: MenuItem[] = [
@@ -9,6 +10,15 @@ const MENU_ITEMS: MenuItem[] = [
   { id: 3, title: 'Gallery', link: '/gallery'},
   { id: 4, title: 'Contact us', link: '/contact-us'}
 ];
+
+const IMAGES: Image[] = [
+  { id: 1, url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg',	alt:'orange tree'},
+  { id: 2, url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg',	alt:'submerged'},
+  { id: 3, url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg',	alt:'look out'},
+  { id: 4, url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg',	alt:'orange tree'},
+  { id: 5, url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg',	alt:'submerged'},
+  { id: 6, url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg',	alt:'look out'}
+]
 
 
 //decorator
@@ -22,13 +32,14 @@ export class AppComponent {
 
   //private static
   menuItems = MENU_ITEMS;
+  images = IMAGES;
 
   //public
   title: string;
   selectedItem : MenuItem;
 
   constructor(){
-    this.title = 'Tour of Heroes';
+    this.title = 'The Jumbotron';
     this.selectedItem = this.menuItems[0];
   }
 
